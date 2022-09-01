bb8f3b5546aec2a0c49ce18081a29002da56f56f
Arthur Estrems <arthurjeu7@gmail.com>
2022-09-01 18:58:22 +0200

    Initial commit

---
+++ src/test/java/fr/projetannuel/gameaddict/GameAddictApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.projetannuel.gameaddict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameAddictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
